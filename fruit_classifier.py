b5a1cd0b69dfc1c785653bbcdd9b218ca5d31628
Wingham1 <matt.wingham97@gmail.com>
2019-12-09 10:50:33 +0800

    Update fruit_classifier.py
    Updated function doc strings

--- fruit_classifier.py
+++ fruit_classifier.py
@@ -7,15 +7,21 @@ import numpy as np
 
 def data_prep(path_to_training_images, img_rows, img_cols, color):
     """
-    A function to get the input image data and the label (as a one hot pixel array) when given a path to a directory containing all the images.
-    This function requires that the images for each class are in a seperate directory and the directory is named after class.
+    A function to preprocess the input data for a CNN.
+    The images are resized, normalised to have pixel values between 0-1, converted into greyscale if required and put into a numpy array.
+    Each class label is turned into a one hot pixel array and added to an ordered numpy array such that the order for the labels is the same as the images.
+    The data is shuffled to make sure each batch is representative of the overall data during training which will reduce overfitting to each batch.
+    This function requires that the images for each class are in a seperate directory.
     
     param:
            - path_to_training_images, a string of the path to the directory containing the images
+           - img_rows, an integer for the number of rows the resized image should have
+           - img_cols, an integer for the number of columns the resized image should have
+           - color, a boolean that is set to true if the image should be in RGB colour space or false for greyscale
     return:
-           - images, an numpy array of images with pixel values normalised to be between 0 and 1.
+           - images, a numpy array of images with pixel values normalised to be between 0 and 1.
              numpy array dimensions are [number of images, number of rows, number of columns, number of chanels]
-           - labels, a list of labels associated with each image (labels are a one hot pixel numpy array [1, 0, 0, ...] or [0, 1, 0, ...], etc)
+           - labels, a numpy array of labels associated with each image (labels are a one hot pixel numpy array [1, 0, 0, ...] or [0, 1, 0, ...], etc)
     """
     
     images = []
@@ -40,7 +46,7 @@ def data_prep(path_to_training_images, img_rows, img_cols, color):
     labels = keras.utils.to_categorical(labels, num_classes=len(os.listdir(path_to_training_images)))
     return images, labels
 
-def build_CNN(color=False):
+def build_CNN(img_rows, img_cols, color=False):
     model = Sequential()
     if color:
         model.add(Conv2D(20, kernel_size=(3, 3), strides=1, activation='relu', input_shape=(img_rows, img_cols, 3)))
@@ -54,15 +60,34 @@ def build_CNN(color=False):
     model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam', metrics=['accuracy'])
     return model
 
-def decode_preditions(predictions, class_names):
+def decode_labels(coded, class_names):
     """
-    A funtion to get the name of the predicted class
+    A funtion to get the name of the class by decoding a one hot pixel array.
+    Uses a list comprehension and boolean indexing.
+    The list comprehension returns the index of the variable with the highest value in each one hot pixel array.
+    That list is then used for boolean indexing with a numpy array to get a list of class_names for each label in coded.
+    
+    Param:
+          - coded, a numpy array of coded labels
+          - class_names, a list of the class_names in the same order they were coded (alphabetical)
+    Return:
+          - numpy array of class names for each label in coded
     """
     
-    return np.array(class_names)[[np.argmax(example) for example in predictions]]
+    return np.array(class_names)[[np.argmax(example) for example in coded]]
 
 def calc_accuracy(pred, real):
-        return sum(pred==real) / len(pred)
+    """
+    A function to calculate the accuracy of a CNN when given a list of predicted classes and a list of the real classes
+    
+    Param:
+          - pred, a numpy array of predicted classes
+          - real, a numpy array of the real classes
+    Return:
+          - Accuracy as a decimal
+    """
+    
+    return sum(pred==real) / len(pred)
 
 if __name__ == '__main__':
 
@@ -78,7 +103,7 @@ if __name__ == '__main__':
     x, y = data_prep(path_to_training_images, img_rows, img_cols, color=is_color)
 
     print('\nbuilding model\n')
-    cnn = build_CNN(color=is_color)
+    cnn = build_CNN(img_rows, img_cols, color=is_color)
 
     print('\ntraining model\n')
     cnn.fit(x, y, batch_size=50, epochs=1, validation_split=0.2)
@@ -100,7 +125,7 @@ if __name__ == '__main__':
 
     print('\ngenerating predictions\n')
     predictions = loaded_cnn.predict(x_test)
-    dec_preds = decode_preditions(predictions, os.listdir(path_to_training_images))
-    dec_ytest = decode_preditions(y_test, os.listdir(path_to_training_images))
+    dec_preds = decode_labels(predictions, os.listdir(path_to_training_images))
+    dec_ytest = decode_labels(y_test, os.listdir(path_to_training_images))
 
     print('\naccuracy =', calc_accuracy(dec_preds, dec_ytest))
